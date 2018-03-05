765435c5b0bd417a48771b1a022f295e8896fb9b
Siddharth Gupta <36362337+luckywilliams@users.noreply.github.com>
2018-03-05 18:39:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// call security file first
+require_once('src/security.php');
+
+// script containing javasctipt or other tags
+$string =' this is some javascritp code block <script type="text/javascritp"> Code.... </script>';
+
+// call Procedural fucntion
+echo ProceduralXssSecuirty($string);
+
+// add a line break
+echo "<br />";
+
+// create new object instance of OopSecurity class
+$security = new OopSecurity();
+
+// call the method and pass the string
+echo $security -> XssSecuirty($string);
+
+?>
---
+++ src/security.php
@@ -0,0 +1,34 @@
+<?php
+// Example : 
+
+//in procedural programming
+
+// define a function
+function ProceduralXssSecuirty($string){
+	
+	// htmlspecialchars — Convert special characters and quotes to HTML entities
+	$string = htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+	
+	//return string
+	return $string;
+}
+
+
+// Example : 
+
+// in object oriented programming
+
+// define a class first 
+class OopSecurity{
+	
+	// define a function
+	public function XssSecuirty($string){
+		
+		// htmlspecialchars — Convert special characters and quotes to HTML entities
+		$string = htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+		//return string
+		return $string;
+		
+	}
+}
+?>
